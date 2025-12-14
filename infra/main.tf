a1c9d30f363d123b030b875144de2a548335cba8
Will Soto <wsoto12@wgu.edu>
2025-12-14 18:53:28 -0500

    Set prod Lambda env KMS CMK

--- infra/main.tf
+++ infra/main.tf
@@ -13,6 +13,9 @@ terraform {
   }
 }
 
+# ----------------------------
+# Variables
+# ----------------------------
 variable "aws_region" {
   type        = string
   description = "AWS region"
@@ -39,28 +42,49 @@ variable "voice_id" {
   default     = "Joanna"
 }
 
+# NEW: Customer-managed KMS key ARN for Lambda env var encryption
+# Example:
+# arn:aws:kms:us-east-1:502487622733:key/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+variable "lambda_env_kms_key_arn" {
+  type        = string
+  description = "Customer-managed KMS key ARN used to encrypt Lambda environment variables"
+  default     = ""
+}
+
 provider "aws" {
   region = var.aws_region
 }
 
+# ----------------------------
+# Locals
+# ----------------------------
 locals {
-  project    = "pixel-learning-tts"
-  fn_name    = var.environment == "beta" ? "PollyTextToSpeech_Beta" : "PollyTextToSpeech_Prod"
-  api_name   = "pixel-learning-tts-${var.environment}-api"
+  project   = "pixel-learning-tts"
+  fn_name   = var.environment == "beta" ? "PollyTextToSpeech_Beta" : "PollyTextToSpeech_Prod"
+  api_name  = "pixel-learning-tts-${var.environment}-api"
   route_path = "/${var.environment}/synthesize"
 
   tags = {
     Project     = local.project
     Environment = var.environment
   }
+
+  # Normalize kms ARN to null if empty string
+  lambda_env_kms_key_arn = trim(var.lambda_env_kms_key_arn) != "" ? trim(var.lambda_env_kms_key_arn) : null
 }
 
+# ----------------------------
+# Package Lambda (zip infra/lambda/)
+# ----------------------------
 data "archive_file" "lambda_zip" {
   type        = "zip"
   source_dir  = "${path.module}/lambda"
   output_path = "${path.module}/lambda.zip"
 }
 
+# ----------------------------
+# IAM Role for Lambda
+# ----------------------------
 resource "aws_iam_role" "lambda_role" {
   name = "${local.fn_name}-role"
 
@@ -76,49 +100,67 @@ resource "aws_iam_role" "lambda_role" {
   tags = local.tags
 }
 
+# ----------------------------
+# IAM Inline Policy (Logs + Polly + S3 env-scoped + KMS for env encryption)
+# NOTE: aws_iam_role_policy does NOT support tags.
+# ----------------------------
 resource "aws_iam_role_policy" "lambda_policy" {
   name = "${local.fn_name}-policy"
   role = aws_iam_role.lambda_role.id
 
-  # NOTE: aws_iam_role_policy does NOT support tags
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "AllowCloudWatchLogs",
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      },
-      {
-        Sid      = "AllowPollySynthesize",
-        Effect   = "Allow",
-        Action   = ["polly:SynthesizeSpeech"],
-        Resource = "*"
-      },
-      {
-        Sid      = "AllowWriteToEnvPrefix",
-        Effect   = "Allow",
-        Action   = ["s3:PutObject"],
-        Resource = "arn:aws:s3:::${var.bucket_name}/polly-audio/${var.environment}/*"
-      },
-      {
-        Sid    = "AllowKmsDecryptForLambdaRuntime",
-        Effect = "Allow",
-        Action = [
-          "kms:Decrypt",
-          "kms:GenerateDataKey"
-        ],
-        Resource = "*"
-      }
-    ]
+    Statement = concat(
+      [
+        # CloudWatch Logs
+        {
+          Sid    = "AllowCloudWatchLogs",
+          Effect = "Allow",
+          Action = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Resource = "*"
+        },
+
+        # Polly synth
+        {
+          Sid      = "AllowPollySynthesize",
+          Effect   = "Allow",
+          Action   = ["polly:SynthesizeSpeech"],
+          Resource = "*"
+        },
+
+        # S3 PutObject ONLY to env prefix (beta/prod isolation)
+        {
+          Sid    = "AllowWriteToEnvPrefix",
+          Effect = "Allow",
+          Action = ["s3:PutObject"],
+          Resource = "arn:aws:s3:::${var.bucket_name}/polly-audio/${var.environment}/*"
+        }
+      ],
+
+      # Only add KMS permissions if lambda_env_kms_key_arn is provided
+      local.lambda_env_kms_key_arn == null ? [] : [
+        {
+          Sid    = "AllowKmsForLambdaEnvEncryption",
+          Effect = "Allow",
+          Action = [
+            "kms:Decrypt",
+            "kms:GenerateDataKey",
+            "kms:DescribeKey"
+          ],
+          Resource = local.lambda_env_kms_key_arn
+        }
+      ]
+    )
   })
 }
 
+# ----------------------------
+# Lambda Function
+# ----------------------------
 resource "aws_lambda_function" "tts" {
   function_name = local.fn_name
   role          = aws_iam_role.lambda_role.arn
@@ -131,6 +173,9 @@ resource "aws_lambda_function" "tts" {
   filename         = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
+  # NEW: force Lambda env var encryption to your CMK (if provided)
+  kms_key_arn = local.lambda_env_kms_key_arn
+
   environment {
     variables = {
       BUCKET_NAME = var.bucket_name
@@ -142,6 +187,9 @@ resource "aws_lambda_function" "tts" {
   tags = local.tags
 }
 
+# ----------------------------
+# API Gateway (HTTP API v2)
+# ----------------------------
 resource "aws_apigatewayv2_api" "http_api" {
   name          = local.api_name
   protocol_type = "HTTP"
@@ -170,13 +218,16 @@ resource "aws_apigatewayv2_route" "route" {
 }
 
 resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowInvokeFromAPIGW-${var.environment}"
+  statement_id  = "AllowInvokeFromAPIGW-${var.environment}-${aws_apigatewayv2_api.http_api.id}"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.tts.function_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
 
+# ----------------------------
+# Outputs
+# ----------------------------
 output "api_invoke_url" {
   value = aws_apigatewayv2_api.http_api.api_endpoint
 }
--- infra/prod.tfvars
+++ infra/prod.tfvars
@@ -1,3 +1,6 @@
 aws_region  = "us-east-1"
 environment = "prod"
+bucket_name = "pixel-learning-tts-wsoto"
 voice_id    = "Joanna"
+
+lambda_env_kms_key_arn = "arn:aws:kms:us-east-1:502487622733:key/<YOUR-CUSTOMER-MANAGED-KEY-ID>"
